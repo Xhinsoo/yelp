e9c4070740966a45c0a33a2d2c49cdcb08a85564
LordNinth <rr621801@gmail.com>
2024-02-22 23:54:06 +0000

    seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Campground = require("../models/campground");
 const cities = require("./cities");
-const{places, descriptors} = require('./seedHelper')
+const { places, descriptors } = require("./seedHelper");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/yelp-camp")
@@ -12,19 +12,21 @@ mongoose
     console.log("error is:", e);
   });
 
-
-const sample = (array) => array[Math.floor(Math.random()*array.length)]  
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-  await Campground.deleteMany({});
+  await Campground.deleteMany({}); //deletes everything in DB and adds new locations
   for (let i = 0; (i = 50); i++) {
     const rand1000 = Math.floor(Math.random() * 1000);
     const camp = new Campground({
       location: `${cities[rand1000].city},${cities[rand1000].state}`,
-      title: `${sample(descriptors)}, ${sample(places)}`
+      title: `${sample(descriptors)}, ${sample(places)}`,
     });
     await camp.save();
   }
 };
 
-seedDB();
+//returns promise bcas its async ()
+seedDB().then(() => {
+  mongoose.connection.close();
+});
