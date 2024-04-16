bc67b8540d1e970caba2a464fbbfaa1309b1966c
LordNinth <rr621801@gmail.com>
2024-04-16 19:08:08 +0100

    show page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,17 +33,28 @@ router.get("/:id", isLoggedIn, isLoggedIn, async (req, res) => {
 
 //render edit page and send put req
 router.get("/:id/edit", isLoggedIn, async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
+  const {id} =req.params;
+  const campground = await Campground.findById(id);
+  if(!campground){
+    req.flash("error","cannot find that campground");
+    return res.redirect("/campground");
+  }
+  if(!campground.author.equals(req.user._id)){
+    req.flash("error","you do not have permission")
+    return res.redirect(`/campground/${id}`)
+  }
   res.render("edit", { campground });
 });
 
 //put update route
 router.put("/:id", isLoggedIn, async (req, res) => {
-  const campground = await Campground.findByIdAndUpdate(
-    req.params.id,
-    req.body.campground
-  );
-  await campground.save();
+  const {id} = req.query;
+  const campground = await Campground.findById(id);
+  if(!campground.author.equals(req.user._id)){
+    req.flash("error","you do not have permission")
+    return res.redirect(`/campground/${id}`)
+  }
+  const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground});
   req.flash("success", "Successfully edited campground");
   res.redirect(`/campground/${campground._id}`);
 });
